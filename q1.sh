dc2b54b04494ae8b87abfd25e8f32a9d1ae72949
roy <roygoldr@gmail.com>
2025-02-06 10:28:43 +0000

    add q1

--- q1.sh
+++ q1.sh
@@ -1,1 +1,8 @@
-This is q1 answer
+awk 'END {print NR}' aliceinwonderland.txt
+3325
+awk '/ Alice / {print}' aliceinwonderland.txt| awk '{for(i=1;i<=NF;i++){if ($i=="Alice") a[$i]++}} END {for (k in a) print a[k], k}' | awk '{print $1}'
+155
+awk '{for(i=1;i<=NF;i++){a[$i]++}} END {for (k in a) print a[k], k}' aliceinwonderland.txt | awk '{if ($1 == 1) print}'
+awk '{for(i=1;i<=NF;i++){a[$i]++}} END {for (k in a) print a[k], k}' aliceinwonderland.txt | sort -n | tail -n 5
+awk '{for(i=1;i<=NF;i++){wordcount++;wordslen+=length($i)}} END {print wordslen/wordcount}' aliceinwonderland.txt 
+4.37187
